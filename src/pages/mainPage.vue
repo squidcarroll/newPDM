ee8b625e6a128e9c6bfbf4539ab89e5be8a8132f
Sid Carroll <squidcarroll@gmail.com>
2019-12-05 17:00:19 -0700

    pushing all files

---
+++ src/pages/mainPage.vue
@@ -0,0 +1,54 @@
+/* eslint-disable */
+<template>
+  <div class="content">
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item md-size-20">
+        <login></login>
+      </div>
+      <div class="md-layout-item">
+        <showData></showData>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import tableify from 'tableify'
+import showData from '../components/showData'
+import login from '../components/login'
+
+export default {
+  name: 'mainPage',
+  components: {
+    showData,
+    login
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+  color: white;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.content {
+  padding: 20px;
+}
+</style>
